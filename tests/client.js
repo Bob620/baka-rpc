a33f98654bce018b3a3d62f1bf93148aa9ea37a6
bob620 <bruder.kraft225@gmail.com>
2019-11-10 17:39:57 -0600

    feat(tests) add automated test cases for basic datatypes

--- tests/client.js
+++ tests/client.js
@@ -1,5 +1,7 @@
 const { BakaRPC, constants } = require('../index.js');
 
+const tests = require('./test.json');
+
 const rpc = new BakaRPC();
 
 rpc.on(constants.errors.STREAM_CLOSED, err => {
@@ -10,11 +12,54 @@ rpc.on(constants.errors.STREAM_ERROR, err => {
 	console.error(err);
 });
 
+async function runTests(tests) {
+	try {
+		for (const {method, expects, params} of tests) {
+			const result = await rpc.request(method, params);
+			if (expects === result)
+				console.error(`(${method}): PASS`);
+			else if (Array.isArray(expects) && Array.isArray(result)) {
+				if (expects.length === result.length && !expects.map(e => result.includes(e)).includes(false))
+					console.error(`(${method}): PASS`);
+				else
+					console.error(`(${method}): FAIL\n\tGot: ${result}\n\tExpected: ${expects}`);
+			} else if (typeof expects === 'object' && typeof result === 'object') {
+				const expectsKeys = Object.keys(expects);
+				const expectsValues = Object.values(expects);
+				const resultKeys = Object.keys(result);
+				const resultValues = Object.values(result);
+
+				if (expectsKeys.length !== resultKeys.length || expectsKeys.map(e => resultKeys.includes(e)).includes(false)) {
+					console.error(`(${method}): FAIL\n\tGot:`);
+					console.error(expects);
+					console.error(`\tExpected:`);
+					return console.error(expects);
+				}
+
+				if (expectsValues.length !== resultValues.length || expectsValues.map(e => resultValues.includes(e)).includes(false)) {
+					console.error(`(${method}): FAIL\n\tGot:`);
+					console.error(expects);
+					console.error(`\tExpected:`);
+					return console.error(expects);
+				}
+
+				console.error(`(${method}): PASS`);
+			} else
+				console.error(`(${method}): FAIL\n\tGot: ${result}\n\tExpected: ${expects}`);
+		}
+	} catch (err) {
+		console.error(err);
+	}
+	process.exit();
+}
+
 async function test() {
 	try {
 		console.error(await rpc.request('test'));
 		console.error(await rpc.request('echo', {str: 'test2'}));
-		console.error(await rpc.request('truthy'));
+		console.error(await rpc.request('true'));
+		console.error(await rpc.request('empty'));
+		console.error(await rpc.request('object'));
 		console.error(await rpc.request('false'));
 		console.error(await rpc.request('falsy'));
 		rpc.notification('test');
@@ -32,4 +77,4 @@ async function test() {
 	}
 }
 
-test();
+runTests(tests);
--- tests/test.js
+++ tests/test.js
@@ -2,21 +2,25 @@ const { spawn } = require('child_process');
 
 const { BakaRPC, constants} = require('../index.js');
 
+const tests = require('./test.json');
+
 const client = spawn('/usr/bin/node', ['./tests/client.js'], {
 	stdio: ['pipe', 'pipe', 'inherit']
 });
 
-//client.stdout.on('data', console.log);
-
 const rpc = new BakaRPC(client.stdout, client.stdin);
 
-
-rpc.on('test', () => {
-	console.log(`(RPC1) Test: test`);
-});
-
+for (const {method, result} of tests)
+	rpc.on(method, () => {
+		if (result !== undefined)
+			if (result === "undefined") {
+				return undefined;
+			} else
+				return result
+	});
+/*
 rpc.on('echo', (str) => {
-	console.log(`(RPC1) Echo: ${str}`);
+	//console.log(`(RPC1) Echo: ${str}`);
 });
 
 rpc.on('false', () => {
@@ -27,17 +31,25 @@ rpc.on('falsy', () => {
 	return '';
 });
 
-
-rpc.on('truthy', () => {
+rpc.on('true', () => {
 	return true;
 });
 
+rpc.on('object', () => {
+	return {test: 'tes2t'};
+});
+
+rpc.on('empty', () => {
+	return {};
+});
+
 rpc.on(constants.errors.STREAM_CLOSED, err => {
-	console.log('(test): ');
-	console.log(err);
+	//console.log('(test): ');
+	//console.log(err);
 });
 
 rpc.on(constants.errors.STREAM_ERROR, err => {
-	console.log('(test): ');
-	console.log(err);
+	//console.log('(test): ');
+	//console.log(err);
 });
+*/
